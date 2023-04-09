4ed368ea1836b76251fddea298bfe9531f322c93
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-09 22:21:20 +0200

    Add User class

--- main/models.py
+++ main/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.TextField(null=True, blank=True)
     
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
