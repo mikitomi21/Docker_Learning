83d78863ea06aa8f86509e0bbbf2f7e257c165de
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-09 20:49:39 +0200

    Add textblock to write a message

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    description = models.TextField(null=True, blank=True)
+    
+    created = models.DateTimeField(auto_now_add=True)
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import MessageForm
 
 # Create your views here.
 
 def home(request):
-    context = {}
-    return render(request, 'home.html', context)
+    form = MessageForm()
+    if request.method == "POST":
+        print(request.POST.get('description'))
+        form = MessageForm(request.POST)
+        if form.is_valid() and request.POST.get('description'):
+            form.save()
+            return redirect('home')
+        
+    context = {'form':form}
+    return render(request, 'main/home.html', context)
 
 def about(request):
     return HttpResponse("Halo halo")
