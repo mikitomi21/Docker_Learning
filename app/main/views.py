292679ca931bca1ed0fc83bc4657af7a5dbc7fd8
Kuba Stachowicz <kubastach2001@gmail.com>
2023-04-09 16:12:02 +0200

    Add new elements

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello World!!!")
+
+def about(request):
+    return HttpResponse("Halo halo")
